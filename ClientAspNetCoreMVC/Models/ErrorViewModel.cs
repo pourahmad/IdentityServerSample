8c5dd4a313aff1441852d7274b769f549b1d62b7
nasser pourahmad <nasserpourahmad@hotmail.com>
2024-02-25 11:00:40 +0330

    Add project files.

---
+++ ClientAspNetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClientAspNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ClientAspNetCoreMVC/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultScheme = "Cookies";
+    option.DefaultChallengeScheme = "oidc";
+})
+.AddCookie("Cookies")
+.AddOpenIdConnect("oidc", option =>
+{
+    option.Authority = "https://localhost:7051/";
+    option.ClientId = "efe06a2e-2081-4362-a966-be477dde2ac6";
+    option.GetClaimsFromUserInfoEndpoint = true;
+    option.SignInScheme = "Cookies";
+    option.Scope.Add("role");
+    option.Scope.Add("email");
+    option.Scope.Add("phone");
+    option.Scope.Add("profile");
+    option.Scope.Add("fullName");
+
+    option.MapInboundClaims = false; // Don't rename claim types
+
+    option.SaveTokens = true;
+});
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
